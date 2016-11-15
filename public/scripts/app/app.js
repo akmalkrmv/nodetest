4167bfcdd763818328b755c007ee91ba935d5a66
Timur Abdurasulov <sver40ck@gmail.com>
2016-11-15 15:31:20 +0500

    update, create (user)

--- public/scripts/app/app.js
+++ public/scripts/app/app.js
@@ -1,40 +1,77 @@
 var app = angular.module("app", []);
 //var User = require("../models/User").User;
+var User = function (username, email) {
+    var self = this;
+
+    self.username = username;
+    self.email = email;
+
+    return self;
+};
 
 app.controller("UserCtrl", ["$scope", "$http", function ($scope, $http) {
 
     loadAll();
 
-    function loadAll() {
-        $http.get("/api/user/").then(function (response) {
-            $scope.users = response.data;
-        });
+    $scope.createUser = function (str) {
+        $scope.title = "Create";
+        $scope.selectedUser = new User();
+        $scope.method = "create()";
     }
-
     $scope.create = function () {
-        $http.post("/api/user/", $scope.newUser).then(function (response) {
-            //$scope.users = response.data;
-            //$scope.newUser = new User();
-        });
-    }
-
-    $scope.update = function (user) {
-        $http.put("/api/user/" + user._id, user).then(function (response) {
-            //$scope.users = response.data;
+        console.log("create query");
+        if (!$scope.selectedUser.email || !$scope.selectedUser.username) {
+            console.log("pnx");
+            return;
+        }
+        $http.post("/api/user/", $scope.selectedUser).then(function (response) {
+            loadAll();
+            $('#userModal').modal('toggle');
         });
     }
 
     $scope.remove = function (event, user) {
         event.preventDefault();
 
-        if (confirm('Are you sure to delete this?'))
+        if (confirm('Are you sure to delete this?')) {
             $http.delete("/api/user/" + user._id).then(function (response) {
-                // $scope.users = response.data;
+                // $scope.users = response.data; // ebaniy zombi
                 var index = $scope.users.indexOf(user);
                 $scope.users.splice(index, 1);
             });
+        }
     }
 
+    $scope.selectUser = function (user, str) {
+        $scope.title = "Update";
+        $scope.selectedUser = user;
+        $scope.method = "update()";
+    }
+    $scope.update = function () {
+        console.log("update query");
+        $http.put("/api/user/" + $scope.selectedUser._id, $scope.selectedUser).then(function (response) {
+            $('#userModal').modal('toggle');
+        });
+    }
+
+    $scope.$watch('users', function (newValue) {
+        if (newValue === undefined) return;
+        $scope.userCount = newValue.length;
+    });
+
+    $scope.save = function () {
+        if (!$scope.selectedUser._id) {
+            $scope.create();
+        } else {
+            $scope.update();
+        }
+    }
+
+    function loadAll() {
+        $http.get("/api/user/").then(function (response) {
+            $scope.users = response.data;
+        });
+    }
 }]);
 
 
