a9e2d2b5eba11a61adf1e1f8f103f820a8e1093b
akmal <akmalkarimov94@gmail.com>
2016-11-15 20:29:36 +0500

    file upload testing

--- models/models.js
+++ models/models.js
@@ -4,19 +4,30 @@ var UserSchema = new mongoose.Schema({
   username: { type: String },
   email: { type: String }
 });
+
 var LanguageSchema = new mongoose.Schema({
   name: { type: String },
   words: [{ type: mongoose.Schema.ObjectId, ref: 'word' }]
 });
+
 var VocabularySchema = new mongoose.Schema({
   image: { data: Buffer, contentType: String },
   words: [{ type: mongoose.Schema.ObjectId, ref: 'word' }]
 });
+
 var WordSchema = new mongoose.Schema({
   text: { type: String },
+  audio: { data: Buffer, contentType: String },
   languageId: { type: mongoose.Schema.ObjectId, ref: 'language' },
   vocabularyId: { type: mongoose.Schema.ObjectId, ref: 'vocabulary' }
 });
+
+LanguageSchema.virtual('wordTexts', {
+  ref: 'word', 
+  localField: '_id',
+  foreignField: 'languageId'
+});
+
 
 var User = mongoose.model('user', UserSchema);
 var Language = mongoose.model('language', LanguageSchema);
